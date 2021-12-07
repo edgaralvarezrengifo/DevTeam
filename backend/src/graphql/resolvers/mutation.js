0f8fc1c296b1384528e1856a98e57a7024d420cd
Edgar Alvarez <edgare192@hotmail.com>
2021-12-07 07:33:03 -0500

    se agrega mutaciones de actualización de proyectos y usuarios

--- backend/src/graphql/resolvers/mutation.js
+++ backend/src/graphql/resolvers/mutation.js
@@ -12,6 +12,23 @@ const Mutation = {
         await Proyecto.updateOne({_id},{$set: {fase}}); 
         return await Proyecto.findById(_id);
     },
+    deleteUser: async(_,{_id})=>{
+        await Proyecto.findByIdAndDelete({_id})
+        return "Delete successful"
+    },
+
+    updateProyecto: async(_,{_id, nombre_proyecto,objetivos_generales,objetivos_especificos,presupuesto})=>{
+        const idd = await Proyecto.findById({_id});
+        await Proyecto.updateOne({_id},{$set: { nombre_proyecto,objetivos_generales,objetivos_especificos,presupuesto}}); 
+        
+        return await Proyecto.findById(_id);
+    },
+
+    updateEstadoProyecto: async(_,{_id, estado})=>{
+        const idd = await Proyecto.findById({_id});
+        await Proyecto.updateOne({_id},{$set: {estado}}); 
+        return await Proyecto.findById(_id);
+    },
 
     createUser: async(_, { name, email, status, user, password, rol })=>{
         const newUser = new User({ name, email, status, user, password, rol })
@@ -21,6 +38,11 @@ const Mutation = {
         const idd = await User.findById({_id});
         User.id="asdasd";
         await User.updateOne({_id},{$set: {name, email}}); // no se a que asignarle este valor, que es donde se hace el update
+        return await User.findById(_id);
+    },
+    updateUserStatus: async(_,{_id, status})=>{
+        const idd = await User.findById({_id});
+        await User.updateOne({_id},{$set: {status}}); 
         return await User.findById(_id);
     },
     deleteUser: async(_,{_id})=>{
